7c1171593b92133ab390cf9034146e024c0d0e2e
mahmoudmabrok <mahmoudmabrok3579@gmail.com>
2019-12-04 07:02:23 +0200

    refactor code

--- app/src/main/java/com/bennyplo/graphics2d/w2/MyView.java
+++ app/src/main/java/com/bennyplo/graphics2d/w2/MyView.java
@@ -8,8 +8,8 @@ import android.view.View;
 
 public class MyView extends View {
     private Paint redPaint; //paint object for drawing the lines
-    private Coordinate[] cube_vertices;//the vertices of a 3D cube
-    private Coordinate[] draw_cube_vertices;//the vertices for drawing a 3D cube
+    private Coordinate[] cucubeVertices;//the vertices of a 3D cube
+    private Coordinate[] drawCubeVertices;//the vertices for drawing a 3D cube
 
     public MyView(Context context) {
         super(context, null);
@@ -20,51 +20,51 @@ public class MyView extends View {
         redPaint.setColor(Color.RED);
         redPaint.setStrokeWidth(2);
         //create a 3D cube
-        cube_vertices = new Coordinate[8];
-        cube_vertices[0] = new Coordinate(-1, -1, -1, 1);
-        cube_vertices[1] = new Coordinate(-1, -1, 1, 1);
-        cube_vertices[2] = new Coordinate(-1, 1, -1, 1);
-        cube_vertices[3] = new Coordinate(-1, 1, 1, 1);
-        cube_vertices[4] = new Coordinate(1, -1, -1, 1);
-        cube_vertices[5] = new Coordinate(1, -1, 1, 1);
-        cube_vertices[6] = new Coordinate(1, 1, -1, 1);
-        cube_vertices[7] = new Coordinate(1, 1, 1, 1);
-        draw_cube_vertices = translate(cube_vertices, 2, 2, 2);
-        draw_cube_vertices = scale(draw_cube_vertices, 40, 40, 40);
-        draw_cube_vertices = rotate(draw_cube_vertices, 45, 1);
-        draw_cube_vertices = rotate(draw_cube_vertices, 45, 0);
-        draw_cube_vertices = rotate(draw_cube_vertices, 80, 2);
-        draw_cube_vertices = rotate(draw_cube_vertices, 30, 1);
-        //  draw_cube_vertices = shear(draw_cube_vertices, 2, 1);
+        cucubeVertices = new Coordinate[8];
+        cucubeVertices[0] = new Coordinate(-1, -1, -1, 1);
+        cucubeVertices[1] = new Coordinate(-1, -1, 1, 1);
+        cucubeVertices[2] = new Coordinate(-1, 1, -1, 1);
+        cucubeVertices[3] = new Coordinate(-1, 1, 1, 1);
+        cucubeVertices[4] = new Coordinate(1, -1, -1, 1);
+        cucubeVertices[5] = new Coordinate(1, -1, 1, 1);
+        cucubeVertices[6] = new Coordinate(1, 1, -1, 1);
+        cucubeVertices[7] = new Coordinate(1, 1, 1, 1);
+        drawCubeVertices = translate(cucubeVertices, 2, 2, 2);
+        drawCubeVertices = scale(drawCubeVertices, 40, 40, 40);
+        drawCubeVertices = rotate(drawCubeVertices, 45, 1);
+        drawCubeVertices = rotate(drawCubeVertices, 45, 0);
+        drawCubeVertices = rotate(drawCubeVertices, 80, 2);
+        drawCubeVertices = rotate(drawCubeVertices, 30, 1);
+        //  drawCubeVertices = shear(drawCubeVertices, 2, 1);
         thisview.invalidate();//update the view
     }
 
-    private void DrawLinePairs(Canvas canvas, Coordinate[] vertices, int start, int end, Paint paint) {
+    private void drawLinePairs(Canvas canvas, Coordinate[] vertices, int start, int end, Paint paint) {
         //draw a line connecting 2 points
         canvas.drawLine((int) vertices[start].x, (int) vertices[start].y,
                 (int) vertices[end].x, (int) vertices[end].y, paint);
     }
 
-    private void DrawCube(Canvas canvas) {//draw a cube on the screen
-        DrawLinePairs(canvas, draw_cube_vertices, 0, 1, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 1, 3, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 3, 2, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 2, 0, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 4, 5, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 5, 7, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 7, 6, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 6, 4, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 0, 4, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 1, 5, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 2, 6, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 3, 7, redPaint);
+    private void drawCube(Canvas canvas) {//draw a cube on the screen
+        drawLinePairs(canvas, drawCubeVertices, 0, 1, redPaint);
+        drawLinePairs(canvas, drawCubeVertices, 1, 3, redPaint);
+        drawLinePairs(canvas, drawCubeVertices, 3, 2, redPaint);
+        drawLinePairs(canvas, drawCubeVertices, 2, 0, redPaint);
+        drawLinePairs(canvas, drawCubeVertices, 4, 5, redPaint);
+        drawLinePairs(canvas, drawCubeVertices, 5, 7, redPaint);
+        drawLinePairs(canvas, drawCubeVertices, 7, 6, redPaint);
+        drawLinePairs(canvas, drawCubeVertices, 6, 4, redPaint);
+        drawLinePairs(canvas, drawCubeVertices, 0, 4, redPaint);
+        drawLinePairs(canvas, drawCubeVertices, 1, 5, redPaint);
+        drawLinePairs(canvas, drawCubeVertices, 2, 6, redPaint);
+        drawLinePairs(canvas, drawCubeVertices, 3, 7, redPaint);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         //draw objects on the screen
         super.onDraw(canvas);
-        DrawCube(canvas);//draw a cube onto the screen
+        drawCube(canvas);//draw a cube onto the screen
 /*
 
         Coordinate[] coordinates = new Coordinate[3];
